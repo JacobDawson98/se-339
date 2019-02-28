a4f67265d442de17418889d27c573f5357ddd351
JacobDawson98 <dawson@iastate.edu>
2019-02-28 11:20:49 -0600

    Feature: Add assignment 3 files before linting

---
+++ FleetManagementServer/driver-microservice/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO driver (id, first_name, last_name, phone_number, vehicle_id) VALUES
+  (0, 'Gigi', 'Wentworth (877-CASH-NOW)', '(555) 000-1234', 0),
+  (1, 'Fu', 'Duh', '(555) 000-4321', 1),
+  (2, 'Joe', 'Schmo', '(555) 000-5858', 2);
